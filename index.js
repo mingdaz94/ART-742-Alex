3097949f82e98dc1cd83254e9f714e81f4d172f0
mingdaz94 <78283700+mingdaz94@users.noreply.github.com>
2021-02-28 18:26:06 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Initialize and add the map
 function initMap() {
   // The location of YaleArchitecture
-  const YaleArchitecture= { lat: 41.3087, lng: 72.9318 };
+  const YaleArchitecture= { lat: 41.3087, lng: -72.9318 };
   // The map, centered at Uluru
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 8,
