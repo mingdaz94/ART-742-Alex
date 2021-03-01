1bf06d7ba66866dc83b6218704950e755065027c
mingdaz94 <78283700+mingdaz94@users.noreply.github.com>
2021-03-01 02:32:56 -0500

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function initMap() {
   });
 
   const image =
-    "https://freight.cargo.site/t/orihttps://d2w9rnfcy7mm78.cloudfront.net/10972416/original_d942326bd3edfa781d6373c84b5b8075.png?1614581393?bc=0ginal/i/3dee9b7f13872c4d12e6fb8459f24cb65cfd02b41fa6c1224e6e8cf2333a8be2/favicon-32x32.png";
+    "https://d2w9rnfcy7mm78.cloudfront.net/10972416/original_d942326bd3edfa781d6373c84b5b8075.png?1614581393?bc=0";
   const beachMarker = new google.maps.Marker({
     position: { lat: 41.3087, lng: -72.9318 },
     map,
