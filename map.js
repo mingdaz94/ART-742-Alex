cbc693c748c0c6d14aa980f20158eaf0558be17d
mingdaz94 <78283700+mingdaz94@users.noreply.github.com>
2021-03-02 00:46:17 -0500

    Update map.js

--- map.js
+++ map.js
@@ -21,11 +21,6 @@ function initMap() {
 
   const image =
     "https://d2w9rnfcy7mm78.cloudfront.net/10972416/original_d942326bd3edfa781d6373c84b5b8075.png?1614581393?bc=0";
-  const Marker = new google.maps.Marker({
-    position: { lat: 41.3087, lng: -72.9318 },
-    map,
-    icon: image,
-  });
 
   // randomly click map to create locations
   function placeMarkerAndPanTo(latLng, map) {
@@ -52,6 +47,28 @@ function initMap() {
     });
     const marker = new google.maps.Marker({
       position: { lat: 41.3087, lng: -72.9318 },
+      map,
+      icon: image,
+      title: "Yale School of Architecture",
+    });
+    marker.addListener("click", () => {
+      infowindow.open(map, marker);
+    });
+
+    const contentString =
+    '<div id="content">' +
+    '<div id="siteNotice">' +
+    "</div>" +
+    '<div id="bodyContent">' +
+    "<p1>This is Yale School of Art </p1>" +
+    "</div>" +
+    "</div>";
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
+      maxWidth: 200,
+    });
+    const marker = new google.maps.Marker({
+      position: { lat: 41.3087806200157, lng: -72.93300509612062 }, 
       map,
       icon: image,
       title: "Yale School of Architecture",
