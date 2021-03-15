3e4694eebede8c7f237d67b2e13ea0e257a98aba
mingdaz94 <78283700+mingdaz94@users.noreply.github.com>
2021-03-15 17:26:07 -0400

    Update map.js

--- map.js
+++ map.js
@@ -1,93 +1,111 @@
-let map;
+var map;
+var InforObj = [];
+var centerCords = {
+                 lat: 41.3087,
+                 lng: -72.9318
+             };
+             var markersOnMap = [{
+                     placeName: "Australia (Uluru)",
+                     LatLng: [{
+                         lat: 41.3087,
+                         lng: -72.9318
+                     }]
+                 },
+                 {
+                     placeName: "Australia (Melbourne)",
+                     LatLng: [{
+                         lat: 41.308242,
+                         lng: -72.930887
+                     }]
+                 },
+                 {
+                     placeName: "Australia (Canberra)",
+                     LatLng: [{
+                         lat: 41.307378,
+                         lng: -72.928732
+                     }]
+                 },
+                 {
+                     placeName: "Australia (Gold Coast)",
+                     LatLng: [{
+                         lat: 41.308325,
+                         lng: -72.927919
+                     }]
+                 },
+                 {
+                     placeName: "Australia (Perth)",
+                     LatLng: [{
+                         lat: -31.951994,
+                         lng: 475.858081
+                     }]
+                 }
+             ];
+     
+             window.onload = function () {
+                 initMap();
+             };
+     
+             function addMarker() {
+             const image = {
+             url:
+             "https://d2w9rnfcy7mm78.cloudfront.net/10972416/original_d942326bd3edfa781d6373c84b5b8075.png?1614581393?bc=0",
+            };
 
-function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    zoom: 10,
-    zoomControl: false,
-    scaleControl: false,
-    mapTypeControl: false,
-    fullscreenControl: false,
-    zoomControl: false,
-    center: { lat: 41.3087, lng: -72.9318 },
-    mapId: '697a8f85be227c39',
-  });
-  setMarkers(map);
-}
-// Data for the markers consisting of a name, a LatLng and a zIndex for the
-// order in which these markers should display on top of each other.
-const beaches = [
-  ["ysoa", 41.3087, -72.9318],
-  ["Coogee Beach", 41.308242, -72.930887],
-  ["Cronulla Beach", 41.307378, -72.928732],
-  ["Manly Beach", -33.80010128657071, 151.28747820854187],
-  ["Maroubra Beach", -33.950198, 151.259302],
-  ["s Beach", 41.308325, -72.927919],
-];
-
-function setMarkers(map) {
-  const image = {
-    url:
-      "https://d2w9rnfcy7mm78.cloudfront.net/10972416/original_d942326bd3edfa781d6373c84b5b8075.png?1614581393?bc=0",
-  };
-  // Shapes define the clickable region of the icon. The type defines an HTML
-  // <area> element 'poly' which traces out a polygon as a series of X,Y points.
-  // The final coordinate closes the poly by connecting to the first coordinate.
-  const shape = {
-    coords: [1, 1, 1, 20, 18, 20, 18, 1],
-    type: "poly",
-  };
-
-  for (let i = 0; i < beaches.length; i++) {
-    const beach = beaches[i];
-    new google.maps.Marker({
-      position: { lat: beach[1], lng: beach[2] },
-      map,
-      icon: image,
-      shape: shape,
-      title: beach[0],
-      zIndex: beach[3],
-    });
-  }
-  
-  const contentString =
-    '<div id="content">' +
-    '<div id="siteNotice">' +
-    "</div>" +
-    '<div id="bodyContent">' +
-    "<p1>This is Yale School of Architecture</p1>" +
-    "</div>" +
-    "</div>";
-    
-  const infowindow = new google.maps.InfoWindow({
-    content: contentString,
-    maxWidth: 200,
-    });
-
-  const marker = new google.maps.Marker({
-    position: { lat: 41.3087, lng: -72.9318 },
-    map,
-    icon: image,
-    title: "Yale School of Architecture",
-    });
-
-    marker.addListener("click", () => {
-      infowindow.open(map, marker);
-    });
+   
+                 for (var i = 0; i < markersOnMap.length; i++) {
+                     var contentString = '<div id="content"><h1>' + markersOnMap[i].placeName +
+                         '</h1><p>Lorem ipsum dolor sit amet, vix mutat posse suscipit id, vel ea tantas omittam detraxit.</p></div>';
+     
+                     const marker = new google.maps.Marker({
+                         position: markersOnMap[i].LatLng[0],
+                         icon: image,
+                         map: map
+                         
+                     });
+     
+                     const infowindow = new google.maps.InfoWindow({
+                         content: contentString,
+                         maxWidth: 200
+                     });
+     
+                     marker.addListener('click', function () {
+                         closeOtherInfo();
+                         infowindow.open(marker.get('map'), marker);
+                         InforObj[0] = infowindow;
+                     });
+                 }
+             }
+     
+             function closeOtherInfo() {
+                 if (InforObj.length > 0) {
+                     InforObj[0].set("marker", null);
+                     /* and close it */
+                     InforObj[0].close();
+                     /* blank the array */
+                     InforObj.length = 0;
+                 }
+             }
+     
+             function initMap() {
+                 map = new google.maps.Map(document.getElementById('map'), {
+                     zoom: 15,
+                     center: centerCords,
+                 });
+                 addMarker();
+             }
     
     
   // randomly click map to create locations
-  map.addListener("click", (e) => {
-    placeMarkerAndPanTo(e.latLng, map);
-  });
+  // map.addListener("click", (e) => {
+  //   placeMarkerAndPanTo(e.latLng, map);
+  // });
   
-  function placeMarkerAndPanTo(latLng, map) {
-    new google.maps.Marker({
-      position: latLng,
-      map: map,
-      icon: image,
-    });
-    map.panTo(latLng);
-  }
+  // function placeMarkerAndPanTo(latLng, map) {
+  //   new google.maps.Marker({
+  //     position: latLng,
+  //     map: map,
+  //     icon: image,
+  //   });
+  //   map.panTo(latLng);
+  // }
 
-
-}
